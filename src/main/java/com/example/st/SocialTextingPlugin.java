ef0f2e35057f11f96af898ab077b6fd5224c849e
aenti7 <wfact@bk.ru>
2026-07-24 21:55:28 +0300

    Create SocialTextingPlugin.java

---
+++ src/main/java/com/example/st/SocialTextingPlugin.java
@@ -0,0 +1,36 @@
+package com.example.st;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SocialTextingPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("SocialTexting включён!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Эта команда только для игроков.");
+            return true;
+        }
+        Player player = (Player) sender;
+
+        if (args.length == 0) {
+            player.sendMessage("§4[Social Texting] §rДоступные режимы:");
+            player.sendMessage("");
+            player.sendMessage("Режим P (оскорбление):");
+            player.sendMessage("  /st p [rus/eng] [ник] [имя]");
+            player.sendMessage("");
+            player.sendMessage("Режим M (старый):");
+            player.sendMessage("  /st m [rus/eng] [ник] [причина]");
+            return true;
+        }
+
+        return STCommand.processSTCommand(player, args);
+    }
+}
